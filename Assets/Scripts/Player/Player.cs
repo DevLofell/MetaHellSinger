f9a75c02c88bbb45e12234971eb8b88dac83c3e0
LeeGyuhyeons <ghl7276@gmail.com>
2024-07-12 10:54:48 +0900

    적 그로기 UI 추가

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -7,68 +7,62 @@ using UnityEngine.SocialPlatforms.Impl;
 
 public class Player : MonoBehaviour
 {
-    //�÷��̾� ü��
+    //플레이어 체력
     public float currHP = 0;
     public float maxHP = 1000;
 
 
-    //�������� �̵�
+    //즉살 공격
     bool onOneShot = false;
     EnemyController_GH nearestEnemy = null;
     float speTime = 0;
 
-    //�ذ� Ư������
+    //스컬 특수공격
     public LayerMask enemyLayer;
     public float stunRadius = 5.0f;
     public GameObject stunEffectFactory;
 
-    #region �÷��̾� �⺻ ������
-    //ī�޶� ȸ�� �ӵ� ��
+    #region 플레이어 기본 움직임
+    //플레이어 회전값
     public float rotSpeed = 200f;
 
-    //�÷��̾� ���콺 ȸ��
+    //플레이어,,
     float mx = 0;
 
-    //�÷��̾� ���ǵ�
+    //플레이어 속도
     public float moveSpeed = 7f;
 
-    //�÷��̾� �̵� ����
+    //방향
     public Vector3 dir;
 
     CharacterController cc;
     #endregion
 
-    #region ���� ����
-    //����
-    //�߷� ����
+    #region 플레이어 점프
+    //중력값
     public float gravity = -4f;
-    //���� �ӷ� ����
+    //플레이어 중력 값
     public float yVelocity = 0;
-    //������ ����
+    //점프높이
     public float jumpPower = 1.7f;
-    //�������� ����
+    //점프 상태
     public bool isJumping = false;
-    //���� ���� ��
+    //점프 횟수
     int nowJumpCount = 0;
-    //�ִ� ���� ��
+    //최대 점프 횟수
     int maxJumpCount = 2;
 
     #endregion
 
-    //���Ÿ�����
-    //���̾ ������
+    //스컬 공격
+    //스컬 공격 프리팹
     public GameObject fireBallFactory;
-    //���̾ ���� ��ġ
+    //스컬 공격 위치
     public GameObject fireBallPos;
-    //���̾ ����
+    //스컬 공격 방향
     public Vector3 fireBallDir;
 
-
-
-    //�ǰ� ��ƼŬ �ý���
-    ParticleSystem ps;
-
-    //�ִϸ�����
+    //플레이어 애니메이터
     private PlayerAnimator playerAnimator;
 
     
@@ -84,17 +78,15 @@ public class Player : MonoBehaviour
     {
         cc = GetComponent<CharacterController>();
         currHP = maxHP;
-        //ps = bulletEffect.GetComponent<ParticleSystem>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (onOneShot)
         {
 
             speTime += Time.deltaTime;
-            //���� ���� �Ÿ�
+            //적과 플레이어의 거리
             float distance;
 
             distance = Vector3.Distance(transform.position, nearestEnemy.transform.position);
@@ -160,12 +152,10 @@ public class Player : MonoBehaviour
         dir.y = 0;
         dir = dir.normalized * scalar;
 
-        //���� ���?
-        //�����̽��� ������ ����
 
         if (!cc.isGrounded)
         {
-            //ĳ���� ���� �ӵ��� �߷� ���� ����
+           
             yVelocity += gravity * Time.deltaTime;
 
         }
@@ -199,54 +189,48 @@ public class Player : MonoBehaviour
 
 
         cc.Move(dir * moveSpeed * Time.deltaTime);
-        //transform.position += dir * moveSpeed * Time.deltaTime;
 
-
-        //�ִϸ��̼�
-        //���� �ִϸ��̼� ����
         playerAnimator.OnMovement(h, v);
 
     }
 
     public void Fire()
     {
-        //���� ���°� Į�� ��
+        //플레이어가 검 상태일 때
         if (playerAnimator.animator.GetInteger("weaponState") == 0)
         {
-            ////Į�� �ִϸ��̼� ����
+            //검 애니메이션을 실행
             playerAnimator.OnSwordAttack();
 
         }
         if (playerAnimator.animator.GetInteger("weaponState") == 1)
         {
-            #region �÷��̾� ���Ÿ�
+            #region 조준선에 정면에 레이를 쏜다
             Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
 
             RaycastHit hitInfo = new RaycastHit();
 
-            //���̾�� �����Ѵ�
+            //스컬 공격을 생성
             GameObject fireBall = Instantiate(fireBallFactory);
-            //���̾�� ��ġ�� ������ġ�� �����?
+            //스컬 공격 위치 설정
             fireBall.transform.position = fireBallPos.transform.position;
-            //���̸� ���µ�...
-            //���𰡿� �ε������� �ε��� ������
+            //앞에 물건이 있을 때
             if (Physics.Raycast(ray, out hitInfo))
             {
 
-                //���̾�� ������ ȭ�� �߾�(����)���� �Ѵ�.
+                //파이어볼을 조준선으로 발사
                 fireBallDir = hitInfo.point - fireBall.transform.position;
-                //������ �̵��Ѵ�.
                 fireBall.transform.forward = fireBallDir.normalized;
 
             }
-            //�ε������� ������ ī�޶� �ٶ󺸴� ��������
+            //조준선에 물건이 없을 때
             else
             {
-                //���̾�� ���� ���������� �ٲ۴�.
+                //그냥 파이어 볼을 앞으로
                 fireBall.transform.forward = Camera.main.transform.forward;
 
             }
-            //2�� �ڿ� ���̾�� �ı��Ѵ�.
+            //파이어볼을 2초 뒤 파괴한다.
             Destroy(fireBall, 2);
             #endregion
 
@@ -261,7 +245,6 @@ public class Player : MonoBehaviour
 
         }
 
-        //���� ���°� Į�� ��
         if (playerAnimator.animator.GetInteger("weaponState") == 0)
         {
             if (Input.GetButtonDown("Fire1"))
@@ -355,31 +338,29 @@ public class Player : MonoBehaviour
                 Vector3 viewportPoint = Camera.main.WorldToViewportPoint(enemy.transform.position);
                 if (viewportPoint.x >= 0 && viewportPoint.x <= 1 && viewportPoint.y >= 0 && viewportPoint.y <= 1 && viewportPoint.z > 0)
                 {
-                    print("��ų�ߵ�");
+                    print("스턴발생");
                     enemy.Stun();
                     GameObject stunEffect = Instantiate(stunEffectFactory);
                     stunEffect.transform.position = enemy.transform.position;
-                    Destroy(stunEffect, 5);
+                    Destroy(stunEffect, 10);
                 }
 
             }
         }
     }
+    //즉살스킬
     void OneShot()
     {
-        //�������� ���̸� ���� ���������ȿ� �ִ� ������ ���� ã�´�
         Collider[] hitColliders = Physics.OverlapSphere(transform.position, stunRadius, enemyLayer);
 
         float nearestDistance = float.MaxValue;
 
-        //ã�� ������ �迭�� �����Ѵ�
         foreach (Collider hitCollider in hitColliders)
         {
             EnemyController_GH enemy = hitCollider.GetComponent<EnemyController_GH>();
             if (enemy != null)
             {
 
-                //����(1. ȭ�鳻�� �־�� �Ѵ� 2. ���� ����� ��)
                 Vector3 viewportPoint = Camera.main.WorldToViewportPoint(enemy.transform.position);
                 if (viewportPoint.x >= 0 && viewportPoint.x <= 1 && viewportPoint.y >= 0 && viewportPoint.y <= 1 && viewportPoint.z > 0)
                 {
@@ -398,12 +379,12 @@ public class Player : MonoBehaviour
         }
         if (nearestEnemy != null)
         {
-            //���� ������ ������ �̵��Ѵ�.
+            //즉살스킬 모드로 한다.
             onOneShot = true;
 
             playerAnimator.GroggyAttack();
 
-            //���� ü���� 0���� �����.
+            //적의 체력을 0으로 만든다.
             nearestEnemy.enemyCurrHP = 0;
         }
     }
