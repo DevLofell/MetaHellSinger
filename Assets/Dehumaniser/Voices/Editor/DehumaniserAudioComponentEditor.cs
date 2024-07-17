2adb9ac09c0a0bf3ade2aceacce80f476d6d54ca
Yang JiWon <ambosing_@naver.com>
2024-07-17 17:37:05 +0900

    Fix: 이동 시 바로 공격하는 버그 수정

---
+++ Assets/Dehumaniser/Voices/Editor/DehumaniserAudioComponentEditor.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+namespace Dehumaniser
+{
+    [CustomEditor(typeof(DehumaniserAudioComponent))]
+    public class DehumaniserAudioComponentEditor : Fabric.Dehumaniser.DehumaniserAudioComponentEditorInternal { }
+}
